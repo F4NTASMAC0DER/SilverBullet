4fabc79cf40c8500a5f5f40f958340413bb2c450
MOHAMMAD <mohamm4dx@outlook.com>
2020-08-07 09:04:09 +0430

    Added websocket block

---
+++ OpenBullet/Views/StackerBlocks/PageBlockWebSocket.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Controls;
+using RuriLib;
+
+namespace OpenBullet.Views.StackerBlocks
+{
+    /// <summary>
+    /// Interaction logic for PageBlockWebSocket.xaml
+    /// </summary>
+    public partial class PageBlockWebSocket : Page
+    {
+        BlockWebSocket vm;
+        public PageBlockWebSocket(BlockWebSocket block)
+        {
+            InitializeComponent();
+            DataContext = vm = block;
+
+            customCookiesRTB.AppendText(vm.GetCustomHeaders());
+        }
+
+        private void wsCommandCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (null == vm) return;
+            vm.Command = (WSCommand)((ComboBox)e.OriginalSource).SelectedIndex;
+
+            switch (vm.Command)
+            {
+                default:
+                    wsCommandTabControl.SelectedIndex = 0;
+                    break;
+
+                case WSCommand.Connect:
+                    wsCommandTabControl.SelectedIndex = 1;
+                    break;
+
+                case WSCommand.Send:
+                    wsCommandTabControl.SelectedIndex = 2;
+                    break;
+            }
+        }
+
+        private void customCookiesRTB_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try { vm.SetCustomCookies(customCookiesRTB.Lines()); } catch { }
+        }
+    }
+}
