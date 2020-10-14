403a3a96a999dac14b2187ba342dddba7f5300de
MOHAMMAD <MOHAMMAD@DESKTOP-PD4SPSD>
2020-10-14 19:39:10 +0330

    changed name

---
+++ OpenBullet/SBIOManager.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using OpenBullet.ViewModels;
+using RuriLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OpenBullet
+{
+    public class SBIOManager
+    {
+        private static JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+
+        public static void SaveSettings(string settingsFile, SBSettingsViewModel settings)
+        {
+            File.WriteAllText(settingsFile, JsonConvert.SerializeObject(settings, Formatting.Indented));
+        }
+
+        public static SBSettingsViewModel LoadSettings(string settingsFile)
+        {
+            return JsonConvert.DeserializeObject<SBSettingsViewModel>(File.ReadAllText(settingsFile));
+        }
+
+        public static void CheckRequiredPlugins(IEnumerable<string> available, Config config)
+        {
+            foreach (var required in config.Settings.RequiredPlugins)
+            {
+                if (!available.Contains(required))
+                {
+                    throw new Exception($"This config requires the plugin {required} which is missing from the Plugins folder and hence cannot be opened!");
+                }
+            }
+        }
+    }
+}
