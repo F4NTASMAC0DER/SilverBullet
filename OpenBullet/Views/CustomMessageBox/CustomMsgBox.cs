60fd9f9283889260276ce29f5ff54284ce1ab7e7
MOHAMMAD <MOHAMMAD@DESKTOP-PD4SPSD>
2020-10-14 12:25:04 +0330

    Added custom message box (material)

---
+++ OpenBullet/Views/CustomMessageBox/CustomMsgBox.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using MahApps.Metro.IconPacks;
+
+namespace OpenBullet.Views.CustomMessageBox
+{
+    public sealed class CustomMsgBox
+    {
+        private const string MessageBoxTitle = "Message";
+
+        /// <summary>
+        /// Displays a message box with OK button
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void Show(string message, bool isRTL = false)
+        {
+            using (var msg = new CustomMsgBoxWindow())
+            {
+                msg.Title = MessageBoxTitle;
+                msg.TitleLabel.Content = MessageBoxTitle;
+                msg.Message.Text = message;
+                msg.BorderBrush = new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                msg.BtnCancel.Visibility = Visibility.Collapsed;
+                if (isRTL)
+                {
+                    msg.FlowDirection = FlowDirection.RightToLeft;
+                }
+                msg.BtnOk.Focus();
+                msg.ShowDialog();
+            }
+        }
+
+
+        /// <summary>
+        ///  Displays a message box with OK button
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="title">The title of the message box</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void Show(string message, string title, bool isRTL = false)
+        {
+            using (var msg = new CustomMsgBoxWindow())
+            {
+                msg.Title = title;
+                msg.TitleLabel.Content = title;
+                msg.Message.Text = message;
+                msg.BorderBrush = new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                msg.BtnCancel.Visibility = Visibility.Collapsed;
+                msg.MsgIcon.Kind = PackIconMaterialKind.InformationOutline;
+                msg.MsgIcon.Foreground = Brushes.LightSteelBlue;
+                if (isRTL)
+                {
+                    msg.FlowDirection = FlowDirection.RightToLeft;
+                }
+                msg.BtnOk.Focus();
+                msg.ShowDialog();
+            }
+        }
+
+        /// <summary>
+        /// Displays an error message box
+        /// </summary>
+        /// <param name="errorMessage">The error error message to display</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void ShowError(string errorMessage, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = "Error";
+                    msg.TitleLabel.Content = "Error";
+                    msg.Message.Text = errorMessage;
+                    msg.BorderBrush = Brushes.Red;
+                    msg.BtnCancel.Visibility = Visibility.Collapsed;
+                    msg.MsgIcon.Kind = PackIconMaterialKind.CloseBoxOutline;
+                    msg.MsgIcon.Foreground = Brushes.Orange;
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Displays an error message box
+        /// </summary>
+        /// <param name="errorMessage">The error error message to display</param>
+        /// <param name="errorTitle">The title of the error message box</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void ShowError(string errorMessage, string errorTitle, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = errorTitle;
+                    msg.TitleLabel.Content = errorTitle;
+                    msg.Message.Text = errorMessage;
+                    msg.BorderBrush = Brushes.Red;
+                    msg.BtnCancel.Visibility = Visibility.Collapsed;
+                    msg.MsgIcon.Kind = PackIconMaterialKind.CloseBoxOutline;
+                    msg.MsgIcon.Foreground = Brushes.Red;
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Displays a warning message box
+        /// </summary>
+        /// <param name="warningMessage">The warning message to display</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void ShowWarning(string warningMessage, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = "Warning";
+                    msg.TitleLabel.Content = "Warning";
+                    msg.Message.Text = warningMessage;
+                    msg.BorderBrush = Brushes.Orange;
+                    msg.BtnCancel.Visibility = Visibility.Collapsed;
+                    msg.MsgIcon.Kind = PackIconMaterialKind.AlertBoxOutline;
+                    msg.MsgIcon.Foreground = Brushes.Orange;
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(warningMessage);
+            }
+        }
+
+        /// <summary>
+        /// Displays a warning message box
+        /// </summary>
+        /// <param name="warningMessage">The warning message to display</param>
+        /// <param name="warningTitle">The title of the error message box</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        public static void ShowWarning(string warningMessage, string warningTitle, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = warningTitle;
+                    msg.TitleLabel.Content = warningTitle;
+                    msg.Message.Text = warningMessage;
+                    msg.BorderBrush = Brushes.Orange;
+                    msg.BtnCancel.Visibility = Visibility.Collapsed;
+                    msg.MsgIcon.Kind = PackIconMaterialKind.AlertBoxOutline;
+                    msg.MsgIcon.Foreground = Brushes.Orange;
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(warningMessage, warningTitle);
+            }
+        }
+
+        /// <summary>
+        /// Displays a message box with a cancel button
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        /// <returns>Message box Result OK or CANCEL</returns>
+        public static MessageBoxResult ShowWithCancel(string message, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = MessageBoxTitle;
+                    msg.TitleLabel.Content = MessageBoxTitle;
+                    msg.Message.Text = message;
+                    msg.BorderBrush = new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                    return msg.Result == MessageBoxResult.OK ? MessageBoxResult.OK : MessageBoxResult.Cancel;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(message);
+                return MessageBoxResult.Cancel;
+            }
+        }
+
+        /// <summary>
+        /// Displays a message box with a cancel button
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="title">The title of the message box</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        /// <returns>Message box Result OK or CANCEL</returns>
+        public static MessageBoxResult ShowWithCancel(string message, string title, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = title;
+                    msg.TitleLabel.Content = title;
+                    msg.Message.Text = message;
+                    msg.BorderBrush = new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                    return msg.Result == MessageBoxResult.OK ? MessageBoxResult.OK : MessageBoxResult.Cancel;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(message);
+                return MessageBoxResult.Cancel;
+            }
+        }
+
+        /// <summary>
+        /// Displays a message box with a cancel button
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="isError">If the message is an error</param>
+        /// <param name="isRTL">(Optional) If true the MessageBox FlowDirection will be RightToLeft</param>
+        /// <returns>Message box Result OK or CANCEL</returns>
+        public static MessageBoxResult ShowWithCancel(string message, bool isError, bool isRTL = false)
+        {
+            try
+            {
+                using (var msg = new CustomMsgBoxWindow())
+                {
+                    msg.Title = MessageBoxTitle;
+                    msg.TitleLabel.Content = MessageBoxTitle;
+                    msg.Message.Text = message;
+                    //msg.TitleBar.Background = isError
+                    //    ? Brushes.Red
+                    //    : new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                    //msg.BorderBrush = isError
+                    //    ? Brushes.Red
+                    //    : new SolidColorBrush(Color.FromRgb(3, 169, 244));
+                    if (isRTL)
+                    {
+                        msg.FlowDirection = FlowDirection.RightToLeft;
+                    }
+                    msg.BtnOk.Focus();
+                    msg.ShowDialog();
+                    return msg.Result == MessageBoxResult.OK ? MessageBoxResult.OK : MessageBoxResult.Cancel;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(message);
+                return MessageBoxResult.Cancel;
+            }
+        }
+    }
+}
---
+++ OpenBullet/Views/CustomMessageBox/CustomMsgBoxWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace OpenBullet.Views.CustomMessageBox
+{
+    /// <summary>
+    /// Interaction logic for CustomMsgBoxWindow.xaml
+    /// </summary>
+    public partial class CustomMsgBoxWindow : IDisposable
+    {
+        public MessageBoxResult Result { get; set; }
+
+        public CustomMsgBoxWindow()
+        {
+            InitializeComponent();
+            Result = MessageBoxResult.Cancel;
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Result = MessageBoxResult.OK;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Result = MessageBoxResult.Cancel;
+            Close();
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+        private void BtnCopyMessage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(Message.Text);
+            }
+            catch { }
+        }
+
+        private void titleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void dragPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Dispose();
+        }
+
+        private void btnQuit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnQuit.BorderBrush = btnQuit.Background = Brushes.DarkRed;
+        }
+
+        private void btnQuit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnQuit.BorderBrush = btnQuit.Background = Brushes.Transparent;
+        }
+    }
+}
